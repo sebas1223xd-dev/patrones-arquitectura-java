0f8016dbdbf289651facc09ecc1bbd56130de24a
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-05-07 20:09:15 -0500

    Create Servidor.java

---
+++ ClienteServidor/Servidor.java
@@ -0,0 +1,13 @@
+public class Servidor {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Servidor iniciado..."
+        );
+
+        System.out.println(
+                "Mensaje recibido: Hola desde el cliente"
+        );
+    }
+}
