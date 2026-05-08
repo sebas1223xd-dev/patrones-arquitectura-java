b669faca344c06aa18f13f4c7bea725f3b753bdc
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-05-07 19:55:27 -0500

    Create ProductoDAO.java

---
+++ LayeredArchitecture/LayeredArchitecture/ProductoDAO.java
@@ -0,0 +1,10 @@
+public class ProductoDAO {
+
+    public Producto obtenerProducto() {
+
+        return new Producto(
+                "Impresora Industrial",
+                2500
+        );
+    }
+}
